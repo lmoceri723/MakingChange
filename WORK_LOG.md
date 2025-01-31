fdb0019688df0fa2059d1e7723b8d19ad88cef68
lmoceri723 <landon.moceri@menloschool.org>
2025-01-31 11:10:49 -0800

    commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date |    Time     |                                                                                                                                                                                                               Update |
+|:-----|:-----------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 1/28 | 10:55-11:25 |                                                                                   I thought about the problem and brainstormed solutions with my classmates. I also cloned the repo and set up the intellij project. |
+| 1/28 | 11:25-11:55 |         I drew a tree on the board of an algorithm I had thought of and worked through how to avoid repetitions. Afterwards, I realized that using a 2D array would allow me to eliminate duplicate recursive calls. |
+| 1/31 | 10:40-11:10 | I coded my recursive approach, changing it a bit by popping coins off the end of the array by using keeping track of an index starting at the length of the coins array and subtracting by 1. I pass all test cases. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MakingChange.java
+++ src/MakingChange.java
@@ -3,7 +3,7 @@
  * given a set of coins, how many ways can you make change for a target amount?
  *
  * @author Zach Blick
- * @author [YOUR NAME HERE]
+ * @author Landon Moceri
  */
 
 public class MakingChange {
@@ -11,7 +11,46 @@ public class MakingChange {
      * TODO: Complete this function, countWays(), to return the number of ways to make change
      *  for any given total with any given set of coins.
      */
+    private static long[][] results;
+
     public static long countWays(int target, int[] coins) {
-        return 0;
+        // Initialize the results array to avoid duplicate recursive calls
+        results = new long[target + 1][coins.length + 1];
+
+        // Fill the array in with -1 to indicate uninitialized values
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j <= coins.length; j++) {
+                results[i][j] = -1;
+            }
+        }
+
+        // Call the recursive helper function
+        return findWays(target, coins, coins.length);
+    }
+
+    // Recursively finds the number of ways to make change given a target and a set of coins
+    private static long findWays(int target, int[] coins, int coinIndex) {
+        // Base cases
+        if (target == 0) {
+            return 1;
+        }
+        // If target is negative or no coins left, return 0
+        if (target < 0 || coinIndex == 0) {
+            return 0;
+        }
+        // Check if the result is already computed
+        if (results[target][coinIndex] != -1) {
+            return results[target][coinIndex];
+        }
+
+        // Store the result in the array
+        // Create two recursive calls, one doing nothing and omitting the last coin
+        // The other subtracting the last coin from the target and not omitting it
+        // This processes all paths of the tree
+        results[target][coinIndex] = findWays(target - coins[coinIndex - 1], coins, coinIndex) +
+                findWays(target, coins, coinIndex - 1);
+
+        // Return the result
+        return results[target][coinIndex];
     }
 }
